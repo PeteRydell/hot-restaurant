3912b53a377f7587d979790b3196770cd9a05d77
Andy Suarez <andysuarez36@gmail.com>
2019-12-05 20:15:36 -0800

    adding waitlist js

---
+++ waitlist.js
@@ -0,0 +1,10 @@
+var waitingArray = [
+    {
+      customerName: "Loaner",
+      customerEmail: "loaner@yahoo.com",
+      phoneNumber: "000-000-0000",
+      customerID: "6"
+    }
+  ];
+  
+  module.exports = waitingArray;
