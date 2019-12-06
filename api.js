64b4552964c0bcf64e50b52b840dad627c8865de
Henry Tum <tujungahenry@gmail.com>
2019-12-05 21:06:10 -0800

    adjustments

--- api.js
+++ api.js
@@ -1,16 +1,3 @@
-// Route to POST to tables object
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "view.html"));
-  });
-  
-  app.get("/add", function(req, res) {
-    res.sendFile(path.join(__dirname, "add.html"));
-  });
-  
-  // Displays all characters
-  app.get("/api/characters", function(req, res) {
-    return res.json(characters);
-  });
 // Tables object
     // custonerName
     // phoneNumber
@@ -20,11 +7,11 @@ app.get("/", function(req, res) {
 // Waitlist object
 
 // Displays all tables
-app.get("/api/tables", function(req, res) {
-    return res.json(tables);
+app.get("/api/tablesReservation", function(req, res) {
+    return res.json(tablesReservation);
   });
 
 // Display waitlist
 app.get("/api/waitlist", function(req, res) {
-    return res.json(tables);
+    return res.json(waitlist);
   });
