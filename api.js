c14b74a6e38422a0bc77b044bd5d7ffc323351c9
Henry Tum <tujungahenry@gmail.com>
2019-12-05 20:41:14 -0800

    Route

--- api.js
+++ api.js
@@ -1,5 +1,16 @@
 // Route to POST to tables object
-
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "view.html"));
+  });
+  
+  app.get("/add", function(req, res) {
+    res.sendFile(path.join(__dirname, "add.html"));
+  });
+  
+  // Displays all characters
+  app.get("/api/characters", function(req, res) {
+    return res.json(characters);
+  });
 // Tables object
     // custonerName
     // phoneNumber
